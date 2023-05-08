be2c70e67bc484a210d4fa14919d81d6ffb7a4ff
Maxi Gottig <maxigottig1985@gmail.com>
2023-05-07 23:35:38 -0300

    Proyecto final

---
+++ main.js
@@ -0,0 +1,45 @@
+// Obtener los elementos del formulario
+const form = document.querySelector('form');
+const montoInput = document.querySelector('#monto');
+const plazoInput = document.querySelector('#plazo');
+const tasaInput = document.querySelector('#tasa');
+const resultadoDiv = document.querySelector('#resultado');
+
+
+// Agregar un evento de envío al formulario
+form.addEventListener('submit', function(e) {
+  // Evitar que el formulario se envíe y se recargue la página
+e.preventDefault();
+
+  // Obtener los valores ingresados por el usuario
+const monto = parseFloat(montoInput.value);
+const plazo = parseInt(plazoInput.value);
+const tasa = parseFloat(tasaInput.value);
+
+  // Calcular el interés ganado
+  const interes = (monto * tasa * plazo) / (365 * 100) + (monto);
+
+  // Mostrar el resultado en la página
+resultadoDiv.textContent = `Tu capital final es: $${interes.toFixed(2)}`;
+mostrarConsejos(interes);
+});
+function mostrarConsejos(interes) {
+let consejos =
+interes < 1000
+    ? 'Consejo: Esperar al vencimiento del plazo fijo y comprar dolares'
+    : interes >= 1001 && interes < 20000
+    ? 'Consejo: Reinvertir durante 30 días más el capital final obtenido'
+    : interes >= 20001 && interes < 100000
+    ? 'Consejo: Esperar al vencimiento del plazo fijo e invertir en Plazo Fijo UVA'
+    : interes >= 100001
+    ? 'Consejo: Esperar al vencimiento del plazo fijo y diversificar el capital final'
+    : 'Ha ocurrido un error al mostrar los consejos.';
+
+document.querySelector('#consejos').textContent = consejos;
+Swal.fire('Aparte del capital final te damos un consejo. Si querés saber más contactanos')
+}
+
+localStorage.setItem('monto', );
+localStorage.setItem('plazo', );
+localStorage.setItem('tasa', );
+localStorage.setItem('resultado', );
